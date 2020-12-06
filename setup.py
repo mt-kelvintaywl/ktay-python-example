d3a4aad779e3fc8cf98f0a884c06d3b25024a745
Kelvin <ktay@moneytree.jp>
2020-12-06 18:55:07 +0900

    chore(setup.py): set url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ PACKAGES = setuptools.find_packages(where="src")
 setuptools.setup(
     name="ktay_python_example",
     version=VERSION,
+    url="https://github.com/mt-kelvintaywl/ktay-python-example",
     author="Kelvin Tay",
     author_email="ktay@moneytree.jp",
     description="Example python package installable from GitHub",
